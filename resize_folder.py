58ae68df492545d2623b96f8ba1eacec4dd72484
Akash Palrecha <akashpalrecha@gmail.com>
2019-04-11 15:40:23 +0530

    Added basic class to plainly resize entire folders Documentation missing This is a quick implementation.

---
+++ resize_folder.py
@@ -0,0 +1,57 @@
+from pathlib import Path
+import numpy as np
+import cv2
+from tqdm import tqdm
+from pathos.multiprocessing import ProcessingPool as Pool
+from pathos.multiprocessing import cpu_count
+
+distribute_process = Pool(cpu_count())
+
+
+class ResizeFolder(object):
+    def __init__(self, image_folder, mask_folder,
+                 output_img_folder, output_mask_folder,
+                 rename_func, height, width):
+        self.IM_FOLDER = Path(image_folder)
+        self.MSK_FOLDER = Path(mask_folder)
+        self.OUTPUT_IMG = Path(output_img_folder)
+        self.OUTPUT_MSK = Path(output_mask_folder)
+        self.OUTPUT_IMG.mkdir(exist_ok=True)
+        self.OUTPUT_MSK.mkdir(exist_ok=True)
+
+        self.rename_func = rename_func
+        self.height = height
+        self.width = width
+
+    def open_image(self, PATH, is_mask=False):
+        if is_mask:
+            return cv2.imread(str(PATH), cv2.IMREAD_GRAYSCALE)
+        else:
+            return cv2.imread(str(PATH))
+
+    def save_image(self, image, img_path, is_mask=False):
+        name = self.rename_func(img_path)
+        if is_mask:
+            cv2.imwrite(str(self.OUTPUT_MSK / name), image)
+        else:
+            cv2.imwrite(str(self.OUTPUT_IMG / name), image)
+
+    def resize_image(self, image):
+        return cv2.resize(image, (self.width, self.height))
+
+    def process(self):
+        # distribute_process.map(self.process_one_image, tqdm( list( self.IM_FOLDER.iterdir() ) ))
+        # distribute_process.map(self.process_one_mask, tqdm( list( self.MSK_FOLDER.iterdir() ) ))
+        for img_path in tqdm(self.IM_FOLDER.iterdir()):
+            self.process_one_item(img_path)
+        for mask_path in tqdm(self.MSK_FOLDER.iterdir()):
+            self.process_one_item(mask_path, is_mask=True)
+
+    def process_one_item(self, img_path, is_mask=False):
+        try:
+            img = self.open_image(img_path, is_mask)
+            img = self.resize_image(img)
+            self.save_image(img, img_path, is_mask)
+            return True
+        except:
+            raise Exception('Problem with processing image')
